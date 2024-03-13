a4895f6efed3a11aca027ae1157e9cec065c568b
nickonometry <nick.andrews@tylertech.com>
2024-03-13 10:34:58 -0400

    added mobile filtering

--- src/lib/mobile-table/mobile-table.svelte
+++ src/lib/mobile-table/mobile-table.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
   import type { IColumnConfiguration } from '@tylertech/forge';
   import MobileServiceCard from '../mobile-service-card/mobile-service-card.svelte';
+  import { filterText } from '../../globalStore';
 
   export let columnConfigurations: IColumnConfiguration[] = [];
   export let data: any[] = [];
+
+  const onSearch = (e) => {
+    let searchTerm = e.target.value.toLowerCase();
+    filterText.update(() => searchTerm);
+  };
 </script>
 
 <div class="grid">
   <forge-text-field class="span-full">
     <forge-icon slot="leading" name="filter_list" external></forge-icon>
-    <input type="text" id="service-search" placeholder="Search for a service" />
+    <input type="text" id="service-search" placeholder="Search for a service" on:input={(e) => onSearch(e)} />
   </forge-text-field>
   {#each data as d, index}
     <MobileServiceCard {index} service={d} />
