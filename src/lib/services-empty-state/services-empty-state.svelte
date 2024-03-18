6145a6f9229d1ff33d8487b3341b2d846e03fd71
nickonometry <nick.andrews@tylertech.com>
2024-03-18 14:58:30 -0400

    fixed issues with empty state responsiveness

--- src/lib/services-empty-state/services-empty-state.svelte
+++ src/lib/services-empty-state/services-empty-state.svelte
@@ -1,9 +1,18 @@
-<div class="padding-16">
+<div class="empty-state-container padding-16 span-full">
   <img src="https://cdn.forge.tylertech.com/v1/images/spot-hero/no-search-results-spot-hero.svg" alt="" />
   <p class="forge-typography--body1">No services match your filter criteria. Please adjust and try again</p>
 </div>
 
 <style>
+  .empty-state-container {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 16px;
+    text-align: center;
+  }
   img {
     width: 100%;
     max-width: 320px;
