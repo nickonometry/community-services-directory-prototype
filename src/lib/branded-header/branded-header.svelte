1d37ac411998b80bd9e58b4d799cea38f23f7881
nickonometry <nick.andrews@tylertech.com>
2024-03-27 10:48:27 -0400

    added sign in button

--- src/lib/branded-header/branded-header.svelte
+++ src/lib/branded-header/branded-header.svelte
@@ -33,7 +33,7 @@
   // ];
 </script>
 
-<forge-toolbar auto-height class="portland-header" no-border>
+<forge-app-bar auto-height class="portland-header" no-border>
   <forge-stack inline alignment="center" slot="start">
     <PortlandLogo />
   </forge-stack>
@@ -48,7 +48,9 @@
       <forge-icon name="menu" external aria-label="Open menu" style="color: white; font-size: 48px"></forge-icon>
     </forge-icon-button>
   </forge-stack> -->
-</forge-toolbar>
+
+  <forge-button slot="end" class="sign-in-button"> Sign in </forge-button>
+</forge-app-bar>
 
 <!-- TO DO - use this on the mocked up municpality website later on -->
 <!-- <forge-modal-drawer bind:this={mobileNavDrawer} direction="right" class="filter-drawer" slot="right" style="z-index: 25">
@@ -62,13 +64,16 @@
 <style>
   .portland-header {
     position: relative;
-    --forge-toolbar-background: #003b4d;
-    --forge-toolbar-start-start-shape: 0;
-    --forge-toolbar-start-end-shape: 0;
-    box-shadow:
-      0 3px 6px rgba(0, 0, 0, 0.16),
-      0 3px 6px rgba(0, 0, 0, 0.23);
-    z-index: 20;
-    --forge-toolbar-padding-block: 8px;
+    --forge-app-bar-height: auto;
+    --forge-app-bar-background: #003b4d;
+    --forge-app-bar-row-padding: 8px;
+
+    &::part(root) {
+      padding-block: 8px;
+    }
+  }
+
+  .sign-in-button {
+    margin-inline-end: 16px;
   }
 </style>
