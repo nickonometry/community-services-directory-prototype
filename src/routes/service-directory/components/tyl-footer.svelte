e79ed0e7d15d1bb699f2eb08844a87cd0c1b1f72
nickonometry <nick.andrews@tylertech.com>
2024-03-29 13:34:33 -0400

    made footer responsive

--- src/routes/service-directory/components/tyl-footer.svelte
+++ src/routes/service-directory/components/tyl-footer.svelte
@@ -1,21 +1,27 @@
-<footer>
-  <forge-stack inline>
-    <a href="#">City of Portland</a>
-    <span> | </span>
-    <a href="#">Contact</a>
-    <span> | </span>
-    <a href="#">Terms of Use</a>
-    <span> | </span>
-    <a href="#">Privacy Policy</a>
-    <span> | </span>
-    <a href="#">© 2024</a>
-  </forge-stack>
-  <div class="logo">
-    <img src="https://cdn.forge.tylertech.com/v1/images/branding/tyler/tyler-empowered-logo-white.svg" alt="Empowered by Tyler Technologies" />
-  </div>
-</footer>
+<div class="container">
+  <footer>
+    <forge-stack inline wrap justify="center">
+      <a href="#">City of Portland</a>
+      <span> | </span>
+      <a href="#">Contact</a>
+      <span> | </span>
+      <a href="#">Terms of Use</a>
+      <span> | </span>
+      <a href="#">Privacy Policy</a>
+      <span> | </span>
+      <a href="#">© 2024</a>
+    </forge-stack>
+    <div class="logo">
+      <img src="https://cdn.forge.tylertech.com/v1/images/branding/tyler/tyler-empowered-logo-white.svg" alt="Empowered by Tyler Technologies" />
+    </div>
+  </footer>
+</div>
 
-<style>
+<style lang="scss">
+  .container {
+    container-type: inline-size;
+  }
+
   footer {
     display: flex;
     justify-content: space-between;
@@ -28,5 +34,23 @@
   a,
   span {
     color: white;
+  }
+
+  @media screen and (max-width: 960) {
+    .footer {
+      flex-direction: column;
+      gap: 24px;
+      text-align: center;
+    }
+  }
+
+  @container (max-width: 860px) {
+    footer {
+      flex-direction: column;
+      gap: 24px;
+      justify-content: center;
+      padding-block: 24px;
+      height: auto;
+    }
   }
 </style>
